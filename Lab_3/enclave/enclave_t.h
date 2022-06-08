269a6b4a15b5b0ce46238177ce42f378f6960570
DaizyCreazy <56557255+DaizyCreazy@users.noreply.github.com>
2022-06-09 00:20:49 +0300

    full version lab3

--- Lab_3/enclave/enclave_t.h
+++ Lab_3/enclave/enclave_t.h
@@ -15,6 +15,11 @@ extern "C" {
 
 void foo(char* buf, size_t len, size_t idx);
 
+sgx_status_t SGX_CDECL sgx_oc_cpuidex(int cpuinfo[4], int leaf, int subleaf);
+sgx_status_t SGX_CDECL sgx_thread_wait_untrusted_event_ocall(int* retval, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_untrusted_event_ocall(int* retval, const void* waiter);
+sgx_status_t SGX_CDECL sgx_thread_setwait_untrusted_events_ocall(int* retval, const void* waiter, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_multiple_untrusted_events_ocall(int* retval, const void** waiters, size_t total);
 
 #ifdef __cplusplus
 }
